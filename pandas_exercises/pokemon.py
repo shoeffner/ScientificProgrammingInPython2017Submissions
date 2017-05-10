c5e278bc2b44e0c5347cf9ebe81f3ba36ef7866b
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-05-10 16:34:16 +0200

    updating homework to new requirements

--- pandas_exercises/pokemon.py
+++ pandas_exercises/pokemon.py
@@ -1,26 +1,27 @@
-import pandas as pd
-
-
 def first_gen(df):
-    '''find the names of all Pokemon from generation 1'''
+    '''find the names of all Pokemon from generation 1
+    returns: series containing names'''
     return df[df.Generation == 1]['Name']
 
 
 def highest_hp(df):
-    '''find the name(s) of the Pokemon with the highest HP'''
+    '''find the name(s) of the Pokemon with the highest HP
+    returns: series containing names'''
     return df[df.HP == df.HP.max()]['Name']
 
 
 def mean_attack_by_type(df):
     '''find the mean attack power of each type (just use Type 1), result will
     contain just type and attack columns
+    returns: data frame with columns ('Type 1', 'Attack')
     '''
-    return df.groupby('Type 1').Attack.mean()
+    return df.groupby('Type 1').Attack.mean().reset_index()
 
 
 def high_defense(df):
     '''find just the Name and Defense rating of Pokemon that have an
     above average (>) Defense.
+    returns: data frame with columns ('Name', 'Defense')
     '''
     return df[df.Defense > df.Defense.mean()][['Name', 'Defense']]
 
@@ -31,5 +32,6 @@ def deduplicated(df):
     can use the # column to tell if they're the same pokemon. Example:
     "Venusaur" has # 3 and there is a "VenusaurMega Venusaur" also
     with # 3, the result should have just "Venusaur"
+    returns: series containing names
     '''
     return df.drop_duplicates('#').Name
--- pandas_exercises/pokemon_test.py
+++ pandas_exercises/pokemon_test.py
@@ -7,16 +7,17 @@ if __name__ == '__main__':
     df = pd.read_csv('Pokemon.csv')
 
     first_gen = pokemon.first_gen(df)
-    print(first_gen)
+    assert isinstance(first_gen, pd.Series), 'first_gen is no Series'
 
     highest_hp = pokemon.highest_hp(df)
-    print(highest_hp)
+    assert isinstance(highest_hp, pd.Series), 'highest_hp is no Series'
 
     mean_attack = pokemon.mean_attack_by_type(df)
-    print(mean_attack)
+    assert isinstance(mean_attack, pd.DataFrame), 'mean_attack is no DataFrame'
 
     high_defense = pokemon.high_defense(df)
-    print(high_defense)
+    assert isinstance(high_defense, pd.DataFrame), 'high_defense is no ' + \
+        'DataFrame'
 
     deduped = pokemon.deduplicated(df)
-    print(deduped)
+    assert isinstance(deduped, pd.Series), 'mean_attack is no DataFrame'
