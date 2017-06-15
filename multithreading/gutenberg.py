ee0302a3e4d23a3adbd259aaeb2e9aabc80ab377
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-06-15 12:44:27 +0200

    using non-cache links to produce better results

--- multithreading/gutenberg.py
+++ multithreading/gutenberg.py
@@ -17,10 +17,18 @@ def process_book(url):
         cache_url = f'https://www.gutenberg.org/cache/epub/{id}/pg{id}.txt'
         detail_page = BeautifulSoup(requests.get(url).text, 'html.parser')
         title = detail_page.find('h1', attrs={'itemprop': 'name'}).string
+        try:
+            textcheck = re.compile('.*text/plain.*')
+            cache_url = ('https:' +
+                         detail_page.find('td', attrs={'content': textcheck})
+                         .find('a', attrs={'type': textcheck})['href'])
+        except (AttributeError, TypeError):
+            pass
         text = requests.get(cache_url).text.lower()
         return title, {vowel: text.count(vowel) for vowel in 'aeiou'}
-    except (TypeError, requests.exceptions.HTTPError):
-        return
+    except (TypeError, requests.exceptions.HTTPError) as e:
+        print(e)
+        return 'error', e
 
 
 def top_20(which='books-last30', limit=20, workers=20):
